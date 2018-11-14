9021a8bf9138236fd4632746ffdbcfcf6b0e700b
Martin Belda <alu0100832211@ull.edu.es>
2018-11-14 15:28:30 +0000

    cosas

--- 7practica/iptables.sh
+++ 7practica/iptables.sh
@@ -12,13 +12,17 @@ $IPT -P FORWARD DROP
 
 $IPT -t nat -P OUTPUT ACCEPT
 $IPT -t nat -P PREROUTING ACCEPT
-$IPT -t nat -p POSTROUTING
+$IPT -t nat -P POSTROUTING ACCEPT
+$IPT -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+# Poner ip de ens6 a los paquetes que salen a internet
+$IPT -t nat -A POSTROUTING -o ens6 -j MASQUERADE
 
 $IPT -N SERVICES
 
 #Permitir trafico en loopback
 $IPT -A INPUT --in-interface lo -j ACCEPT
-
+#Permitir respuesta de conexiones establecidas
+$IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 #Cadena para gestionar los servicios
 $IPT -A INPUT -j SERVICES
 #Aceptar conexiónes desde la red interna a los puertos 80 (web) y 22 (servicio SSH en el FW)
@@ -26,14 +30,9 @@ $IPT -A INPUT -j SERVICES
 $IPT -A SERVICES --in-interface ens3 -p tcp --dport 22 -j ACCEPT
 #servidor web
 $IPT -A SERVICES --in-interface ens3 -p tcp --dport 80 -j ACCEPT
-#impresora 
+#impresora
 $IPT -A SERVICES -m iprange --src-range 192.168.2.1-192.168.2.254 -p tcp --dport 631 -j ACCEPT
 
-#Permitir respuesta de conexiones establecidas
-$IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-$IPT -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+
 $IPT -A FORWARD -p tcp --dport 8080 -j ACCEPT
-
-$IPT -t nat -A POSTROUTING -o ens6 -j MASQUERADE
-
